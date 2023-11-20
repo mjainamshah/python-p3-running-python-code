892212e86142b2a70f3d70855dd44c5a1cab699d
mjainamshah <jainam.shah@student.moringaschool.com>
2023-11-20 16:04:42 +0300

    COMPLETED

---
+++ lib/app.py
@@ -0,0 +1,6 @@
+# print ("HELLO WORLD!")
+# print("Hello Moringa!", end=" ")
+# print("Hello Kelvin!", end="!! ")
+# print("Hello Jainam!", end="!!!\n")
+
+print ("Hello World! Pass this test, please.")
